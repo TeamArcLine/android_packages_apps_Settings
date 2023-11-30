a9e8c278801f20cdf8822f839f2a9a62a9894e80
maxwen <max.weninger@gmail.com>
2023-11-30 07:17:44 +0100

    Settings: Add ArcLab to dashboard
    Change-Id: Ie18a29e62a651b4b5c74510493dc291135f5361c
    Signed-off-by: S I P U N <sipunkumar85@gmail.com>

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -480,4 +480,7 @@ public class Settings extends SettingsActivity {
     public static class PreviouslyConnectedDeviceActivity extends SettingsActivity { /* empty */ }
 
     public static class ScreenTimeoutActivity extends SettingsActivity { /* empty */ }
+
+    // ArcLab Additoon
+    public static class ArcLabActivity extends SettingsActivity {}
 }
